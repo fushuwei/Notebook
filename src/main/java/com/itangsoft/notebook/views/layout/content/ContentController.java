c6a4657996e4c0e63f71b3b3b9995f1d5756bb5a
fushuwei <331044761@qq.com>
2024-01-06 15:45:03 +0800

    优化代码

--- src/main/java/com/itangsoft/notebook/views/layout/content/ContentController.java
+++ src/main/java/com/itangsoft/notebook/views/layout/content/ContentController.java
@@ -2,12 +2,9 @@ package com.itangsoft.notebook.views.layout.content;
 
 import com.github.nalukit.nalu.client.component.annotation.AcceptParameter;
 import com.github.nalukit.nalu.client.component.annotation.Controller;
-import com.github.nalukit.nalu.client.exception.RoutingInterceptionException;
 import com.itangsoft.notebook.Routes;
 import com.itangsoft.notebook.Slots;
 import com.itangsoft.notebook.base.BaseComponentController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Content Controller
@@ -22,21 +19,14 @@ public class ContentController
     extends BaseComponentController<IContentComponent>
     implements IContentComponent.Controller {
 
-    private static final Logger logger = LoggerFactory.getLogger(ContentController.class);
-
     String fileName;
 
     public ContentController() {
     }
 
     @AcceptParameter("fileName")
-    public void setFileName(String fileName) throws RoutingInterceptionException {
-        try {
-            this.fileName = fileName;
-        } catch (NumberFormatException e) {
-            logger.error("参数 fileName 无效", e);
-            throw new RoutingInterceptionException(this.getClass().getCanonicalName(), Routes.ROUTE_HOME);
-        }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
     }
 
     public String getFileName() {
